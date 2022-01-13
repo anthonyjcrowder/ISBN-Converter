1a3bff69fbb038938cb2e46da3e7dbb26abc3658
Anthony Crowder <anthonyjcrowder@icloud.com>
2022-01-13 17:33:12 -0500

    Code has errors, but utilizes array for capturing ibsn 13. Deploying code to not use array, returning to this idea if fails

--- src/ISBNConverter.java
+++ src/ISBNConverter.java
@@ -38,6 +38,42 @@ public class ISBNConverter {
 		return (sum % 11 == 0);
 	}
 
+	public static boolean ValidISBN13(String isbn){
+
+		// create an array that is the length of the scan
+		int[] userScan = new int[isbn.length()];
+		if (userScan.length != 13)
+			return false;
+
+		int sum = 0;
+
+		int j = 1;
+		int k = 3;
+
+		// loop through scan to capture each digit as it's own position in array userScan
+		for (int i = 0; i < userScan.length; i++) {
+
+			// capture each digit in userInput
+			int loc = userScan[i];
+
+			//IMPORTANT. Makes sure that loop knows to only look for values between 0-10. Still need to figure out how to calculate x as last digit
+			if (loc < '0' || loc > '9')
+			{
+				throw new IllegalArgumentException("Invalid input");
+			}
+
+			//check last digit for x
+			char lastdigit = userScan.[i];
+
+			if (lastdigit == 'X') {
+				sum += 10;
+			} else
+				sum += (isbn.charAt(9) - 4);
+		}
+
+		return (sum % 11 == 0);
+	}
+
 	public static void main (String[] args){
 
 		Scanner in = new Scanner(System.in);
